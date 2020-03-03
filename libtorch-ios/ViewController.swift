c30b71c836aacecf75abbea3ce081c5c5b90f866
dnnagy <nagy@chromeshift.com>
2020-03-03 11:26:17 +0100

    Implement timeit()

--- libtorch-ios/ViewController.swift
+++ libtorch-ios/ViewController.swift
@@ -8,6 +8,24 @@
 
 import UIKit
 
+// Helper function to measure time
+func timeit(_ f: () -> Void, loops: UInt) {
+    var runtimesMillis: [Double] = []
+    
+    for _ in 1...loops {
+        let start = Date()
+        f()
+        let end = Date()
+        runtimesMillis.append(end.timeIntervalSince(start))
+    }
+    
+    let total = runtimesMillis.reduce(0, +)
+    let avg = Double(total)/Double(loops)
+    let devs = runtimesMillis.map({ (entry) in return abs(entry-avg)})
+    let avgdevs = devs.reduce(0, +)/Double(loops)
+    print(String(format: "Average: %.2fms std. dev: %.2fms", avg*1000, avgdevs*1000))
+}
+
 class ViewController: UIViewController {
     
     func testCNNBaseline() {
